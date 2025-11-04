949f0227b8e866d1a49d876f8bd7293a236248ac
nikatopu <nikatopu@gmail.com>
2025-11-04 16:30:10 +0400

    CLOSE/ Redirect all users to new portfolio

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {dirname} from "path";
+import { dirname } from "path";
 import { fileURLToPath } from "url";
 var __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -7,13 +7,13 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(__dirname + "/public"));
-app.set('views', __dirname + '/public');
-app.set('view engine', 'ejs');
+app.set("views", __dirname + "/public");
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("main.ejs");
-})
+  res.redirect(301, "https://www.nikatopu.dev/");
+});
 
 app.listen(port, () => {
-    console.log(`Listening on port: ${port}`);
-})
+  console.log(`Listening on port: ${port}`);
+});
